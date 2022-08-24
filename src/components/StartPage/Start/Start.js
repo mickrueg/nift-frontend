b5deb4684c028fbfe93e8bbe78a7e92b5a9edd55
mickrueg <mickrueg@gmail.com>
2022-08-23 19:17:26 -0600

    Updated ease in on start page

--- src/components/StartPage/Start/Start.js
+++ src/components/StartPage/Start/Start.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Start.css';
 import Waves from '../../WaveMotion/Waves/Waves';
 import PausePlay from '../../WaveMotion/PausePlay/PausePlay';
 
 const Start = () => {
+
+    const [startBodyTop, setStartBodyTop] = useState('blur')
+    const [startBodyMiddle, setStartBodyMiddle] = useState('blur')
+    const [startBodyBottom, setStartBodyBottom] = useState('blur')
+
+    useEffect(()=>{
+      setTimeout(() => {
+        setStartBodyTop('clear')
+      }, 100);
+    })
+    useEffect(()=>{
+      setTimeout(() => {
+        setStartBodyMiddle('clear')
+      }, 1100);
+    })
+    useEffect(()=>{
+      setTimeout(() => {
+        setStartBodyBottom('clear')
+      }, 1400);
+    })
+
     return (
         <div className='Start-page'>
           <div className='Start-container'>
             <div className='Start-body'>
-              <h1>The future<br></br>
-              <b>says "hello"</b></h1>
+              <div className={startBodyTop}>
+                <h1>The future<br></br>
+                <b>says "hello"</b></h1>
+              </div>
               <br></br>
-              <h4>New to NFTs? <b><u>Start here</u></b></h4>
+              <div className={startBodyMiddle}>
+                <h4>New to NFTs? <b><u>Start here</u></b></h4>
+              </div>
               <br></br>
               <br></br>
               <br></br>
-              <h4>Login to your <b><u>account</u></b></h4>
-              <h4>NFTs in the <b><u>news</u></b></h4>
+              <div className={startBodyBottom}>
+                <h4>Login to your <b><u>account</u></b></h4>
+                <h4>NFTs in the <b><u>news</u></b></h4>
+              </div>
             </div>
           </div>
           <Waves />

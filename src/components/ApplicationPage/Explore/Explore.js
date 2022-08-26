17ab72159f4c5bb272e7d7c27f3cb6d78a801825
mickrueg <mickrueg@gmail.com>
2022-08-26 11:59:56 -0600

    Added icons for Explore NFTs page

--- src/components/ApplicationPage/Explore/Explore.js
+++ src/components/ApplicationPage/Explore/Explore.js
@@ -9,6 +9,8 @@ const Explore = () => {
 
     //setState
     const [loadingNFTSearch, setLoadingNFTSearch] = useState(true)
+    const [searchNFT, setSearchNFT] = useState('fruit')
+    const [displayResults, setDisplayResults] = useState()
 
     const navigate = useNavigate()
     //Import Context
@@ -22,6 +24,8 @@ const Explore = () => {
         loginForm, setLoginForm
     } = useContext(AppContext)
 
+
+    //On Page Load
     useEffect(()=>{
         setNavbar("Navbar-page")
         setNavExplore('navbar-item')
@@ -30,9 +34,33 @@ const Explore = () => {
         setNavSaved('navbar-item not-selected')
     })
 
+    //Functions
+
+    function displaySearchNFTResults(data){
+        const results = data.search_results.map((nft, index)=>{
+            const nftImageURL = nft.cached_file_url
+            const nftName = nft.name
+            const nftDescription = nft.description
+            
+            if(nftImageURL===null || nftImageURL.slice(-3)==='mp4' || !nftImageURL){
+
+            } else {
+                console.log(nftDescription.slice(0,10)+' '+nftImageURL)
+                return(
+                    <div className='Explore-NFT-box' key={index}>
+                        <img className='Explore-NFT-box-image' src={nftImageURL} alt={nftDescription}/>
+                    </div>
+                )
+            }
+        }
+        )
+        return results
+    }
+
+    //When search keyword changes
     useEffect(()=>{
         setLoadingNFTSearch(true)
-        axios.get('https://api.nftport.xyz/v0/search?chain=all&text=fruit',
+        axios.get(`https://api.nftport.xyz/v0/search?chain=all&text=${searchNFT}`,
             {headers:
                     {
                     'Content-Type': 'application/json',
@@ -42,20 +70,33 @@ const Explore = () => {
         )
             .then(res=>{
                 setLoadingNFTSearch(false)
-                console.log(res.data)
+                setDisplayResults(
+                    displaySearchNFTResults(res.data)
+                )
             })
         //REACT_APP_NFTPORT
-    },[])
+    },[searchNFT])
+
+    function handleSearchSubmit(){
+        
+    }
 
     return (
         <div className='Explore-page'>
             <div className='Explore-container'>
-                <h2>EXPLORE <b>NFTs</b></h2>
-                <hr></hr>
-                <form className='Explore-search-form'>
-                    <input type="text" className='Explore-search-input'></input>
-                    <button type="submit" className='Explore-search-button'>SEARCH</button>
-                </form>
+                <div className='Explore-form-container'>
+                    <div className='Explore-title'>
+                        <h2>EXPLORE <b>NFTs</b></h2>
+                        <hr></hr>
+                    </div>
+                    <form className='Explore-search-form' onSubmit={handleSearchSubmit}>
+                        <input type="text" className='Explore-search-input'></input>
+                        <button type="submit" className='Explore-search-button'>SEARCH</button>
+                    </form>
+                </div>
+                <div className='Explore-results-container'>
+                    {displayResults}
+                </div>
             </div>
         </div>
     );
